e2ab305feac7b309e9315793504ada68340bc292
tr-pyramidanatomy <tr.prof06@gmail.com>
2024-11-01 10:20:56 +0100

    0.3.0

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: path.resolve("src", 'lib/index.js'),
+      name: 'test-modal-thra',
+      // the proper extensions will be added
+      fileName: (format) => `test-modal-thra.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'react',
+        },
+      },
+    },
+  },
   plugins: [react()],
 })
+
