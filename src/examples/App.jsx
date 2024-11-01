8203f4459fc4fe24522911258e0d8525df5c18b5
tr-pyramidanatomy <tr.prof06@gmail.com>
2024-11-01 09:46:24 +0100

    first commit

---
+++ src/examples/App.jsx
@@ -0,0 +1,33 @@
+import './App.scss'
+import { useState } from 'react'
+import Modal from '../lib'
+
+const App = () => {
+    const [modalIsDisplayed, setModalIsDisplayed] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setModalIsDisplayed(true)
+    }
+
+    return (
+        <section className="container">
+            <h1 className="title">Thra Basic Modal React</h1>
+            <form onSubmit={handleSubmit}>
+                <button>Click me</button>
+            </form>
+            <Modal
+                isDisplayed={modalIsDisplayed}
+                onCloseModal={() => setModalIsDisplayed(false)}
+                content={
+                    <div id="confirmation" className="modal">
+                        <p>This is a modal</p>
+                        <p>Click on 'X' button to close me</p>
+                    </div>
+                }
+            />
+        </section>
+    )
+}
+
+export default App
